629def2c94c20b01158cd501cda395a987fa2f1a
glauberratti <glauber.ratti.l@gmail.com>
2023-03-29 18:20:05 -0300

    docs: add documentation for update opening

--- handler/response.go
+++ handler/response.go
@@ -44,6 +44,11 @@ type ShowOpeningResponse struct {
 	Data    schemas.OpeningResponse `json:"data"`
 }
 
+type UpdateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+
 type ListOpeningsResponse struct {
 	Message string                    `json:"message"`
 	Data    []schemas.OpeningResponse `json:"data"`
